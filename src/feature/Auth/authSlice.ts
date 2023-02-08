df21aefbb6b6bfae7c12e5974affb3899d53316e
Dong Le <61934131+dongle47@users.noreply.github.com>
2023-02-08 19:24:09 +0700

    routing login page

--- src/feature/Auth/authSlice.ts
+++ src/feature/Auth/authSlice.ts
@@ -1,12 +1,32 @@
+import { RootState } from './../../app/store';
 import { createSlice } from "@reduxjs/toolkit";
+
+
+interface AuthState{
+    user: User | null
+}
+interface User{
+    username: string;
+    password: string
+}
+
+const initialState:AuthState = {
+    user: null
+}
 
 const authSlice = createSlice({
     name:'auth',
-    initialState: "",
+    initialState,
     reducers: {
 
     }
 })
+
+//actions
+export const authActions = authSlice.actions
+
+//selector
+export const selectUser = (state:RootState) => state.auth.user
 
 //reducers
 const authReducer = authSlice.reducer
--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MasterLayout from "../pages/MasterLayout";
+import Login from "../feature/Auth/Login";
 
 const AppRoutes: React.FC = () => {
   return (
     <>
       <Routes>
         <Route path="/*" element={<MasterLayout />} />
+
+        <Route path="/auth" element={<Login />} />
       </Routes>
     </>
   );
